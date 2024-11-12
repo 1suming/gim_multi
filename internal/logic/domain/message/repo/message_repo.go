e1c37409a0b0a6bb77bc541244235b2ab869f53c
skysunshine <1531430343@qq.com>
2024-11-12 11:15:14 +0800

    modify

--- internal/logic/domain/message/repo/message_repo.go
+++ internal/logic/domain/message/repo/message_repo.go
@@ -5,6 +5,9 @@ import (
 	"gim/internal/logic/domain/message/model"
 	"gim/pkg/db"
 	"gim/pkg/gerrors"
+	"gim/pkg/logger"
+	"github.com/jinzhu/gorm"
+	"go.uber.org/zap"
 )
 
 type messageRepo struct{}
@@ -48,8 +51,19 @@ func (d *messageRepo) ListBySeq(userId, seq, limit int64) ([]model.Message, bool
 
 // ListBySeq 根据类型和id查询大于序号大于seq的消息
 func (d *messageRepo) ListBySeqAndTargetId(userId, seq, limit int64, targetId int64) ([]model.Message, bool, error) {
-	DB := db.DB.Table(d.tableName(userId)).
-		Where("user_id = ? and seq > ? and target_id=?", userId, seq, targetId).Order("seq ASC")
+	//DB := db.DB.Table(d.tableName(userId)).
+	//	Where("user_id = ? and seq > ? and target_id=?", userId, seq, targetId).Order("seq ASC")
+	logger.Logger.Info("ListBySeqAndTargetId", zap.Any("userId:", userId), zap.Any("limit:", limit), zap.Any("targetId:", targetId))
+	var DB *gorm.DB
+	if seq == 0 {
+		//这里我们改变原有的含有，原来为0表示从来没有拉取过消息，要从最开始拉取。
+		//这里修改为拉取最新的，这样可以避免每次都拉取全部消息。
+		DB = db.DB.Table(d.tableName(userId)).
+			Where("user_id = ? and target_id=?", userId, targetId).Order("seq DESC") //逆序排序
+	} else {
+		DB = db.DB.Table(d.tableName(userId)).
+			Where("user_id = ? and seq < ? and target_id=?", userId, seq, targetId).Order("seq DESC")
+	}
 
 	var totalCount int64
 	err := DB.Count(&totalCount).Error
